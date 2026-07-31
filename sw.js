2803da70cb5c284fd6839f165ffe0075f15852ce
aashirwadsahani2008-hue <aashirwadsahani2008@gmail.com>
2026-07-31 17:12:39 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'buildtrack-v1';
+const urlsToCache = [
+  '/Rangmahal-/',
+  '/Rangmahal-/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
